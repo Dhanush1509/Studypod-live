670c3a60e9613c504771b24d0fb02947c545ee43
Dhanush1509 <s.munidhanush15@gmail.com>
2021-05-25 10:43:13 +0530

    sidebar

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{ useContext,useState} from 'react'
 import ClassContext from "../context/information/classContext"
+import HomeIcon from "@material-ui/icons/Home";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import {BiHomeAlt} from "react-icons/bi";
+import { MdLiveTv } from "react-icons/md";
+
 const SideBar = () => {
+    const [sidebarClass,setSidebarClass]=useState("sidebar__drawer")
+    const changeToLarge=()=>{
+        setSidebarClass("sidebar__drawer__long")
+    }
+    const changeToSmall = () => {
+      setSidebarClass("sidebar__drawer");
+    };
     return (
-        <div className="sidebar__drawer">
-            
+      <div
+        className={sidebarClass}
+        onMouseEnter={changeToLarge}
+        onMouseLeave={changeToSmall}
+      >
+        <div>
+          <BiHomeAlt className="sidebar__icons" />
+        
         </div>
-    )
+   <div>
+   
+   <MdLiveTv className="sidebar__icons" />
+   </div>
+       
+      </div>
+    );
 }
 
 export default SideBar
